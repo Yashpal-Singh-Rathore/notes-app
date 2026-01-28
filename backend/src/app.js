90866ec686ec06a0b25cc49b7d02c1fe3afb8b7f
Yashpal <singhyashpal52001@gmail.com>
2026-01-28 15:38:16 +0530

    enabled cors for frontend production url

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://notes-frontend-alpha-sable.vercel.app",
+    ],
     credentials: true,
   }),
 );
